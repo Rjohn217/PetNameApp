769412142cffca45f5db73dc936e5787c3717a94
Austin <austinjohnson1203@gmail.com>
2023-07-31 08:05:09 -0400

    Initialized Spring Boot for Backend

---
+++ back-end/src/test/java/com/namefurme/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namefurme.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
