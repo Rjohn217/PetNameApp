c925ba8ce07da2ac118917ac7395f86cac68d589
Austin <austinjohnson1203@gmail.com>
2023-08-02 01:06:58 -0400

    made pets class

---
+++ back-end/src/main/java/pets.java
@@ -0,0 +1,13 @@
+public class pets {
+    
+    public String breed;
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+}
