72223827db9346dc83ed6ec006320102e8b0628e
Austin <austinjohnson1203@gmail.com>
2023-08-05 23:09:06 -0400

    Updated Header

--- front-end/namefurme/src/components/Header.jsx
+++ front-end/namefurme/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1>Welcome to NameFurMe!</h1>
+      <h1>NameFurMe</h1>
       <nav>
         {/* Add navigation links here */}
       </nav>
