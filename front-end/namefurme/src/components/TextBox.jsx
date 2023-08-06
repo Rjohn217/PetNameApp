87f7b3ae2610a5575517f5bd1dc99fef3fed386f
Austin <austinjohnson1203@gmail.com>
2023-08-06 11:03:24 -0400

    Updated textbox

--- front-end/namefurme/src/components/TextBox.jsx
+++ front-end/namefurme/src/components/TextBox.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 const buttonStyle = {
   backgroundColor: '#00BEEF',
+  marginTop: '10px',
 };
 
 const TextBox = () => {
   return (
     <div className="bg-secondary p-3 rounded">
-      <textarea className="form-control mb-2" name="Name This Pet" cols="30" rows="3"></textarea>
+      <textarea 
+      className="form-control mb-2" 
+      name="Name This Pet" 
+      cols="32" 
+      rows="3"
+      placeholder= "What is a good name for this pet?">
+      </textarea>
       <br />
       <button className="btn btn-info" style={buttonStyle}>Submit</button>
     </div>
