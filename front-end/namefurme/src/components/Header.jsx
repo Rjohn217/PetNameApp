962f3b67816dd87da6352c88818e824b4ca6f346
Austin <austinjohnson1203@gmail.com>
2023-08-06 11:24:55 -0400

    Updated header style

--- front-end/namefurme/src/components/Header.jsx
+++ front-end/namefurme/src/components/Header.jsx
@@ -2,31 +2,33 @@ import React from 'react';
 import pawprint from '../assets/pawprint.png';
 
 const Header = () => {
-
   const headerStyle = {
     fontFamily: "'Farsan', cursive",
     display: 'flex',
     alignItems: 'center',
+    justifyContent: 'center',
+    position: 'fixed',
+    top: 0,
+    left: 0, // Add this to position the header at the left of the viewport
+    width: '100%',
+    background: 'whitesmoke',
     color: '#00BEEF',
-    textShadow: '1px 1px 2px rgba(0, 0, 0, 0.35)'
+    padding: '10px',
+    zIndex: 1,
+    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
   };
 
   const imageStyle = {
     marginLeft: '10px',
-    width: '75px',
-    height: '75px',
-  };
-
-  const headingContainerStyle = {
-    display: 'flex',
-    alignItems: 'center',
+    width: '40px',
+    height: '40px',
   };
 
   return (
     <header style={headerStyle}>
-      <div style={headingContainerStyle}>
-      <h1>NameFurMe</h1>
-      <img src={pawprint} alt="dog paw" style={imageStyle} />
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <h1 style={{ margin: 0 }}>NameFurMe</h1>
+        <img src={pawprint} alt="dog paw" style={imageStyle} />
       </div>
       <nav>
         {/* Add navigation links here */}
