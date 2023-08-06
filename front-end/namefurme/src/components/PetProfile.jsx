4ddfb643b78538ba323719ccb4277217d3f3013a
Austin <austinjohnson1203@gmail.com>
2023-08-06 12:02:32 -0400

    Added petprofile placeholder

--- front-end/namefurme/src/components/PetProfile.jsx
+++ front-end/namefurme/src/components/PetProfile.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import dancingKitty from '../assets/dancingkitty.gif';
 
 const PetProfile = () => {
+  const profileStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '20px',
+    borderRadius: '10px',
+    backgroundColor: '#f1f1f1',
+    marginTop: '-55px',
+    
+  };
+
+  const imageStyle = {
+    width: '300px',
+    height: '400px',
+    objectFit: 'cover',
+    borderRadius: '5%',
+    border : '4px solid #00BEEF',
+  };
+
+  const descriptionStyle = {
+    marginTop: '20px',
+    textAlign: 'center',
+  };
+
   return (
-    <div>
-      <h3>Pet Profile</h3>
-      {/* Add pet's gallery and description here */}
+    <div style={profileStyle}>
+      <img src={dancingKitty} alt="Dancing Kitty" style={imageStyle} />
+      <div style={descriptionStyle}>
+        <p>Baby boy black kitten, loves to dance</p>
+      </div>
     </div>
   );
 };
