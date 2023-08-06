5a662d7b8c2a7b57caec27ea83213a038ae8da14
Austin <austinjohnson1203@gmail.com>
2023-08-06 11:26:29 -0400

    Moved footer component

--- front-end/namefurme/src/components/Footer.jsx
+++ front-end/namefurme/src/components/Footer.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
 const Footer = () => {
+  const footerStyle = {
+    fontFamily: "'Farsan', cursive",
+    textAlign: 'center',
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    background: 'whitesmoke',
+    padding: '10px',
+    boxShadow: '0px -2px 4px rgba(0, 0, 0, 0.2)',
+  };
+
   return (
-    <footer>
+    <footer style={footerStyle}>
       <p>&copy; 2023 NameFurMe. All rights reserved.</p>
     </footer>
   );
