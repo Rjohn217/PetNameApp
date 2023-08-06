817d67e4590c6553616ac5bb431a95a1f4d89966
Austin <austinjohnson1203@gmail.com>
2023-08-05 23:46:26 -0400

    Added logo and styles to header

--- front-end/namefurme/src/components/Header.jsx
+++ front-end/namefurme/src/components/Header.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import pawprint from '../assets/pawprint.png';
 
 const Header = () => {
+
+  const headerStyle = {
+    fontFamily: "'Farsan', cursive",
+    display: 'flex',
+    alignItems: 'center',
+    color: '#00BEEF',
+    textShadow: '1px 1px 2px rgba(0, 0, 0, 0.35)'
+  };
+
+  const imageStyle = {
+    marginLeft: '10px',
+    width: '75px',
+    height: '75px',
+  };
+
+  const headingContainerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+  };
+
   return (
-    <header>
+    <header style={headerStyle}>
+      <div style={headingContainerStyle}>
       <h1>NameFurMe</h1>
+      <img src={pawprint} alt="dog paw" style={imageStyle} />
+      </div>
       <nav>
         {/* Add navigation links here */}
       </nav>
