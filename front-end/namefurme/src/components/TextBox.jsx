4f44d7a12f46822108b908638924946299c5aa9d
Austin <austinjohnson1203@gmail.com>
2023-08-06 10:57:43 -0400

    changed background and added bootstrap

--- front-end/namefurme/src/components/TextBox.jsx
+++ front-end/namefurme/src/components/TextBox.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+const buttonStyle = {
+  backgroundColor: '#00BEEF',
+};
+
 const TextBox = () => {
   return (
-    <div>
-      <textarea name="Name This Pet" id="" cols="30" rows="3"></textarea>
-      <br/>
-      <button>Submit</button>
+    <div className="bg-secondary p-3 rounded">
+      <textarea className="form-control mb-2" name="Name This Pet" cols="30" rows="3"></textarea>
+      <br />
+      <button className="btn btn-info" style={buttonStyle}>Submit</button>
     </div>
   );
 };
