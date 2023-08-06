436e983df8fa061d79a244d5bce8c363dc400f62
Austin <austinjohnson1203@gmail.com>
2023-08-05 22:56:31 -0400

    Created footer component

---
+++ front-end/namefurme/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2023 NameFurMe. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
